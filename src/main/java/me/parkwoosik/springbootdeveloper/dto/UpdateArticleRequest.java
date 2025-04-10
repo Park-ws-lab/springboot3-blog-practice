575dd815aa1b450765b9f9be5c147f41c600edb7
Park-ws-lab <qkrdntlr0126@naver.com>
2025-04-10 23:18:32 +0900

    add blog view

---
+++ src/main/java/me/parkwoosik/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.parkwoosik.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // 기본생성자 추가
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성차 추가
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
